4175cc6e644dea495b5e02d07b6cfb5a99dc0da0
bummer6799 <108254959+bummer6799@users.noreply.github.com>
2023-01-11 16:11:35 +0800

    1

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import webview
+import tkinter as tk
 
-YouTubeVideo = input('Enter YouTube video link: \n')
-webview.create_window("YouTube", YouTubeVideo, width=320, height=240)
+def save_input():
+    global input_text
+    input_text = input_box.get()
+    print("Input saved as:", input_text)
+    root.destroy()
+# Create the main window
+root = tk.Tk()
+root.title("YouTubeWindow")
+
+# Create a label to prompt the user for input
+tk.Label(root, text="Enter video link:").pack()
+
+# Create a text box for the user to input
+input_box = tk.Entry(root)
+input_box.pack()
+
+# Create a button to save the user's input
+save_button = tk.Button(root, text="Play", command=save_input)
+save_button.pack()
+
+# Start the main loop
+root.mainloop()
+
+webview.create_window("YouTube", input_text, width=800, height=600)
 webview.start()
